39b23e099be63075dcd17b3bc0eca980a955c05b
MikeGambino <dan.k-2011@yandex.ru>
2025-11-08 07:56:44 +0400

    update sql scripts and add test data

--- db/add_roles.sql
+++ db/add_roles.sql
@@ -4,8 +4,8 @@ VALUES
 (2, 'ROLE_PATIENT'),
 (3, 'ROLE_DOCTOR');
 
-INSERT INTO users(password, username, email, surname, name, patronymic, gender)
-VALUES ('$2a$10$7ZP5PJuzhGoikMiJM4OOX.HVUuPlwn8DsmZoIXpXuwuhtaypyM04u', 12345, 'dan.k-2011@yandex.ru', 'Кузнецов', 'Даниил', 'Андреевич', 'Мужской');
+INSERT INTO users(password, username, email, surname, name, patronymic, gender, birthday)
+VALUES ('$2a$10$7ZP5PJuzhGoikMiJM4OOX.HVUuPlwn8DsmZoIXpXuwuhtaypyM04u', 12345, 'dan.k-2011@yandex.ru', 'Кузнецов', 'Даниил', 'Андреевич', 'Мужской', '2001-11-07');
 
 INSERT INTO user_role(user_id, role_id)
-VALUES (1, 1);
+VALUES (1, 1), (1, 3);
---
+++ db/test_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO public.doctor(id, surname, name, patronymic, description, image, start_work_date, end_work_date, is_enabled)
+VALUES (1, 'Иванов', 'Алексей', 'Петрович', 'Врач-терапевт', 'default-doc.png', '2025-11-07', null, true);
+
+INSERT INTO public.patient(id, surname, name, patronymic, beneficiary, rh_factor, blood_type)
+VALUES  (1, 'Иванов', 'Алексей', 'Петрович', true, true, 'A(II)');
+
+INSERT INTO public.specialization(id, title, description)
+VALUES (1, 'Терапевт', 'Терапевт — это врач широкого профиля, который занимается выявлением, лечением и профилактикой заболеваний внутренних органов.');
+
+INSERT INTO public.treatment(id, specialization_id, title, description, price, beneficiary_discount, discount)
+VALUES 
+    (1, 1, 'Первичный осмотр', 'Прием терапевта включает первичный осмотр с использованием методов пальпации, перкуссии и аускультации, сбор сведений о симптомах и истории болезни.', 1000, 10, 10),
+    (2, 1, 'Повторный приём', 'Повторный прием терапевта требуется для наблюдения пациента и корректировки ранее назначенного лечения.', 700, 0, 0);
+	
+INSERT INTO public.schedule(id, doctor_id, start_work, end_work, is_enabled)
+VALUES (1, 1, '2028-06-30 09:00:00', '2028-06-30 09:30:00', true);
